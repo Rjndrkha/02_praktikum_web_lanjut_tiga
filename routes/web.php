33ed2e7a6ae6fa1c2cc51a2bbdef6a777572239f
Rjndrkha <75615789+Rjndrkha@users.noreply.github.com>
2021-02-26 13:54:34 +0700

    MY CHANGE

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\NewsController;
+use App\Http\Controllers\ProgramController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,10 +32,29 @@ Route::get('/article/{id}', function ($id) {
 
 
 });
+/*
 //Practicum 2
-
-use App\Http\Controllers\PageController;
 
 Route::get('/',[PageController::class,'index']);
 Route::get('/about',[PageController::class,'about']);
 Route::get('/article/{id}',[PageController::class,'article']);
+*/
+
+//Practicum 3
+Route::get('/',[PageController::class,'index']);
+
+Route::group(['prefix' => 'products'], function () {
+    Route::get('/', [ProductController::class, 'index'])->name('products.index');
+    Route::get('/{products}', [ProductController::class, 'showProducts'])->name('products.showproducts');
+  });
+  
+  Route::get('/news', [NewsController::class, 'index'])->name('news.index');
+  Route::get('/news/{news}', [NewsController::class, 'show'])->name('news.show');
+  
+  Route::group(['prefix' => 'program'], function () {
+    Route::get('/{program}', [ProgramController::class, 'listProgram'])->name('program.list');
+  });
+  
+  Route::get('/about', [AboutController::class, 'index'])->name('aboutus.index');
+  Route::resource('contact', ContactController::class);
+  
